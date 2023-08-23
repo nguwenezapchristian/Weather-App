672e18b43c080e705ed09fef021c5c88e49c3d35
Nguweneza P Christian <nguwenezacc982@gmail.com>
2023-08-23 15:54:47 +0200

    added a function to process and get the required data

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
-const location = "london".toLowerCase;
-const api_key = "d1613c04ca574939968172503232208"
+const city = "kigali";
+const api_key = "d1613c04ca574939968172503232208";
+
+getWeatherData(city);
+
 async function getWeatherData(location) {
     try {
         let link = `https://api.weatherapi.com/v1/current.json?key=${api_key}&q=${location}`;
         const reponse = await fetch(link);
-        const weatherData = reponse.json();
-        console.log(weatherData);
+        const weatherData = await reponse.json();
+        const extractedData = processData(weatherData);
+        
+        console.log(extractedData);
     } catch (err) {
+        if (err == 'Parameter q is missing.') {
+            console.log('input the location');
+        }
         console.log(err);
     }
+}
+
+
+function processData(data) {
+    const processedData = {
+        town: data.location.name,
+        country: data.location.country,
+        lon: data.location.lon,
+        lat: data.location.lat, 
+        icon: data.current.condition.icon,
+        text: data.current.condition.text,
+        last_updated: data.current.last_updated,
+        temp_c: data.current.temp_c,
+        temp_f: data.current.temp_f,
+        humidity: data.current.humidity,
+        
+    }
+    return processedData;
 }
